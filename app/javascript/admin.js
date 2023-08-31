872178001165910591b40e29671109e84dcf71ef
Kevin_makalister <74493602+yazid120@users.noreply.github.com>
2023-08-30 23:40:12 -0500

    feat: Add Dashboard featchers & style

---
+++ app/javascript/admin.js
@@ -0,0 +1,7 @@
+// Entry point for the build script in your package.json
+import "@hotwired/turbo-rails";
+import "./controllers";
+import 'flowbite';
+import "flowbite/dist/flowbite.turbo.js";
+import "./admin/dark-mode"
+import "./admin/sidebar"
