c74ef715e477f772530c753957efc685ecee95fd
rafikfartas <rafikawm8@gmail.com>
2023-09-05 12:04:33 +0100

    add .yarn/ and .yarnrc.yml to gitignore

--- app/controllers/admin/dashboard_controller.rb
+++
@@ -1,5 +0,0 @@
-class Admin::DashboardController < Admin::AdminController
-  def index
-  end
-
-end
--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,4 @@
-class Admin::AdminController < ApplicationController
+class AdminController < ApplicationController
   before_action :authenticate_admin!
   layout "admin"
 
