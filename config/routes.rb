b32759d05b4a6f2e13827e91924e1e4141758aa1
wanisramdani <crazzydocteure@gmail.com>
2023-09-20 09:56:08 +0100

    feat: add admin devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
-  devise_for :admins
+  
+  devise_for :admins, path: 'admins', controllers: {
+    sessions: 'admins/sessions'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
